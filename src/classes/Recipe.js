c9ea549ed0fc63d53c5a1c5612846693690f258b
Alex Kio <alexandermartelkio@gmail.com>
2021-05-15 14:35:03 -0400

    Build out Recipe class and constructor

---
+++ src/classes/Recipe.js
@@ -0,0 +1,12 @@
+class Recipe {
+  constructor(thisRecipe) {
+    this.id = thisRecipe.id;
+    this.image = thisRecipe.image;
+    this.ingredients = thisRecipe.ingredients;
+    this.instructions = thisRecipe.instructions;
+    this.name = thisRecipe.name;
+    this.tags = thisRecipe.tags;
+  }
+}
+
+export default Recipe;
