4e7328fbbe81562426acc98a9c4c0ad2997018ef
Sydney Bear <sbear3915@gmail.com>
2021-05-15 16:53:09 -0600

    Deleteconsole logs and comments

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -19,27 +19,15 @@ class Recipe {
     }, []);
   }
   calculateCostOfIngredients(ingredients) {
-    //reduce to find
-    console.log('THE RECIPE OBJECT', this.ingredients)
-    console.log('INGREDIENTS', ingredients)
-
     const totalCost = ingredients.reduce((acc, item) => {
       let currentIngredient = this.ingredients.find(ingredient => ingredient.id === item.id)
-      console.log('CURRENT INGREDIENT', currentIngredient)
       if(currentIngredient){
-        console.log('amount', currentIngredient.quantity.amount)
-        console.log('estimated cost', item.estimatedCostInCents)
         acc += currentIngredient.quantity.amount * item.estimatedCostInCents
       }
       return acc;
     }, 0);
     return totalCost;
   }
-
-// pass in ingredient = [{id:, cost:}]
-// this.ingredients = [{id:, amount:}]
-// if ingredient[index].id = ingredients[index].id
-// return += ingredient[index].cost * ingredients[index].amount
   returnDirections() {
     const directions = this.instructions.map(item => `${item.number}: ${item.instruction}`);
     return directions.join(' ')
