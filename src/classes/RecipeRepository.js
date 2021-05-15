935156c7d6b05b8b63dd7ca03abf11ad0338f66a
Alex Kio <alexandermartelkio@gmail.com>
2021-05-15 19:32:35 -0400

    Update RecipeRepository constructor

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,11 +1,6 @@
 class RecipeRepository {
-  constructor(thisRecipe) {
-    this.id = thisRecipe.id;
-    this.image = thisRecipe.image;
-    this.ingredients = thisRecipe.ingredients;
-    this.instructions = thisRecipe.instructions;
-    this.name = thisRecipe.name;
-    this.tags = thisRecipe.tags;
+  constructor(recipes) {
+    this.cookbook = recipes;
   }
 }
 
