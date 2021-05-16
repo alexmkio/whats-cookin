1fb04d027e78cea4cf980e4c531b8f59d6edac67
Sydney Bear <sbear3915@gmail.com>
2021-05-16 15:27:49 -0600

    Delete unused variable declarations

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -2,13 +2,12 @@ import { expect } from 'chai';
 import Ingredient from '../src/classes/Ingredient';
 
 describe('Ingredient', () => {
-  let ingredient1, ingredient2, ingredient3, ingredient4, ingredient5
+  let ingredient1, ingredient2, ingredient3
   beforeEach(() => {
     ingredient1 = new Ingredient(9079, 'dried cranberries', 921);
     ingredient2 = new Ingredient(11935, 'catsup', 666);
     ingredient3 = new Ingredient(12151, 'pistachio', 813);
-    ingredient4 = new Ingredient(11821, 'red sweet peppers', 1027);
-    ingredient5 = new Ingredient(6615, 'vegetable stock', 613);
+
   })
 
   it('should be a function', () => {
@@ -30,7 +29,7 @@ describe('Ingredient', () => {
   it('should have a cost', () => {
     expect(ingredient1.estimatedCostInCents).to.equal(921);
     expect(ingredient2.estimatedCostInCents).to.equal(666);
-    expect(ingredient3.estimatedCostInCents).to.not.equal(666)
+    expect(ingredient3.estimatedCostInCents).to.not.equal(666);
   })
 
 
