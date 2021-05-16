a084f8a5c3762bf25e27991125abf4fd9161cfd7
Alex Kio <alexandermartelkio@gmail.com>
2021-05-15 20:00:31 -0400

    Make filterByTag method

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -2,6 +2,10 @@ class RecipeRepository {
   constructor(recipes) {
     this.cookbook = recipes;
   }
+  filterByTag(tag) {
+    const filtered = this.cookbook.filter(item => item.tags.includes(tag));
+    return filtered
+  }
 }
 
 export default RecipeRepository;
