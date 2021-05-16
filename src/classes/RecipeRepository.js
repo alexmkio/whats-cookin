336aae86801c30c3308a4e5f9f694aa351db19c0
Alex Kio <alexandermartelkio@gmail.com>
2021-05-15 20:06:21 -0400

    Create filterByNameOrIng method testing for name

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -3,8 +3,12 @@ class RecipeRepository {
     this.cookbook = recipes;
   }
   filterByTag(tag) {
-    const filtered = this.cookbook.filter(item => item.tags.includes(tag));
-    return filtered
+    const filteredByTag = this.cookbook.filter(item => item.tags.includes(tag));
+    return filteredByTag
+  }
+  filterByNameOrIng(nameOrIng) {
+    const filteredByName = this.cookbook.filter(item => item.name.includes(nameOrIng));
+    return filteredByName
   }
 }
 
