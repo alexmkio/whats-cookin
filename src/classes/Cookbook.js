47a3314173de463d110e8b8fd9bcb5c0b06f4c30
Alex Kio <alexandermartelkio@gmail.com>
2021-05-16 17:03:29 -0400

    Change Class name

---
+++ src/classes/Cookbook.js
@@ -0,0 +1,15 @@
+class Cookbook {
+  constructor(recipes) {
+    this.cookbook = recipes;
+  }
+  filterByTag(tag) {
+    const filteredByTag = this.cookbook.filter(item => item.tags.includes(tag));
+    return filteredByTag
+  }
+  filterByNameOrIng(nameOrIng) {
+    console.log(ingredientsData)
+   
+  }
+}
+
+export default Cookbook;
