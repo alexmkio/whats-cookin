4b633df707538a401ed5dfa0c910ecc2891ee866
Alex Kio <alexandermartelkio@gmail.com>
2021-05-16 19:47:41 -0400

    Create User test file

---
+++ test/User-test.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import User from '../src/classes/User';
+
+describe('User', () => {
+  // let 
+  // beforeEach(() => {
+
+  // })
+
+  it('should be a function', () => {
+    expect(User).to.be.a('function');
+  });
+
+})
