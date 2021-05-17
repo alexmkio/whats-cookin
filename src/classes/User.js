fde3e5fc0a8b25b244fb19c80f97c9d4f9f19694
Alex Kio <alexandermartelkio@gmail.com>
2021-05-16 20:10:59 -0400

    Create determineFavorite method

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,5 +1,14 @@
 class User {
   constructor() {
+    this.favoriteRecipes = [];
+  }
+  determineFavorite(recipe) {
+    if (!this.favoriteRecipes.includes(recipe)) {
+      this.favoriteRecipes.push(recipe)
+    } else {
+      const index = this.favoriteRecipes.indexOf(recipe);
+      this.favoriteRecipes.splice(index, 1);
+    }
   }
 }
 
