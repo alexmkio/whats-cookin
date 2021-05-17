20153af2bec091e2a5acb2871a697323660c37e6
Sydney Bear <sbear3915@gmail.com>
2021-05-16 19:49:43 -0600

    Start building recipe card section function

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,19 @@
-import './styles.css';
+// import './styles.css';
 import apiCalls from './apiCalls';
 
+//query selectors
+const recipeCardsSection = document.getElementById('recipeCards');
 
+//global variables
+const cookbook = new Cookbook()
 
+//event listeners
 
+//functions
+// function updateRecipeCardSection() {
+//   Cookbook.
+//   recipeCardsSection.innerHTML +=
+// }
+
+// updateRecipeCardSection();
 console.log('Hello world');
