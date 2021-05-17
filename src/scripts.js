7640ade277f0ef7285a6ea25a8b0a7209b3bee55
Sydney Bear <sbear3915@gmail.com>
2021-05-17 15:05:29 -0600

    Add recipeData import to script so we can use the data

--- src/scripts.js
+++ src/scripts.js
@@ -1,20 +1,22 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 import Cookbook from '../src/classes/Cookbook';
+import recipeData from '../src/data/recipes';
 
 //query selectors
 const recipeCardsSection = document.getElementById('recipeCards');
 
 //global variables
-const cookbook = new Cookbook()
+const cookbook = new Cookbook(recipeData)
+console.log(cookbook.cookbook[0])
 
 // event listeners
 
 // functions
 // function updateRecipeCardSection() {
-//   Cookbook.
+//   cookbook.
 //   recipeCardsSection.innerHTML +=
 // }
-//
+
 // updateRecipeCardSection();
 console.log('Hello world');
