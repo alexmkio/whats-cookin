86c4c4d8bb195470220b253f8ed4a52588cdfd9c
Sydney Bear <sbear3915@gmail.com>
2021-05-17 18:36:02 -0600

    Finish the html and css structure

--- src/scripts.js
+++ src/scripts.js
@@ -18,7 +18,7 @@ function updateRecipeCardSection() {
     recipeCardsSection.innerHTML += `<section class="recipe-card">
       <div class="image-section">
         <img class="recipe-image" src="${recipe.image}">
-        <h1 class="recipe-name">${recipe.name}</h1>
+        <h3 class="recipe-name">${recipe.name}</h3>
       </div>
       <div class="icon-section">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><path d="M0,0h24v24H0V0z" fill="none"/><path d="M0,0h24v24H0V0z" fill="none"/></g><g><path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"/></g></svg>
