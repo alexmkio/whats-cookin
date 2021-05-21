a3caf7b812ccaa6229c4472e733c30537fdbb6b5
Alex Kio <alexandermartelkio@gmail.com>
2021-05-21 16:05:05 -0400

    Create fetch requests

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,17 @@
-// Your fetch requests will live here!
+export const users = () => {
+  return fetch("http://localhost:3001/api/v1/users")
+    .then(response => response.json())
+    .then(data => data)
+} 
 
+export const recipes = () => {
+  return fetch("http://localhost:3001/api/v1/recipes")
+    .then(response => response.json())
+    .then(data => data)
+} 
 
-console.log('I will be a fetch request!')
+export const ingredients = () => {
+  return fetch("http://localhost:3001/api/v1/ingredients")
+    .then(response => response.json())
+    .then(data => data)
+} 
