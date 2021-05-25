8f5d2bf4fa342cb4d0d2894a0e294cc6342c18e4
Alex Kio <alexandermartelkio@gmail.com>
2021-05-25 14:23:26 -0400

    Add catch error alert

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,16 @@
+const message = "The website is failing to load some of it's resources. Did you follow the installation instructions completely? Did you clone down, install, and start the API repo? Do better!"
+
 export const getIngredientsData = () =>
   fetch("http://localhost:3001/api/v1/ingredients")
-    .then(response => response.json());
+    .then(response => response.json())
+    .catch(err => alert(message))
 
 export const getRecipesData = () =>
   fetch("http://localhost:3001/api/v1/recipes")
-    .then(response => response.json());
+    .then(response => response.json())
+    .catch(err => alert(message))
 
 export const getUsersData = () =>
   fetch("http://localhost:3001/api/v1/users")
-    .then(response => response.json());
+    .then(response => response.json())
+    .catch(err => alert(message))
